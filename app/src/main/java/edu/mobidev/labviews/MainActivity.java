716e63c2981b3d53050676b2b45236862462991e
itsoverhere <courtneyngo@gmail.com>
2015-09-06 11:21:48 +0800

    Added change button functionality

--- app/src/main/java/edu/mobidev/labviews/MainActivity.java
+++ app/src/main/java/edu/mobidev/labviews/MainActivity.java
@@ -1,17 +1,48 @@
 package edu.mobidev.labviews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonChange;
+    TextView tvLabel;
+    ImageView ivPicture;
+
+    // the boolean isFlipped will help us check if the views have changed
+    boolean isFlipped = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonChange = (Button) findViewById(R.id.button_change);
+        tvLabel = (TextView) findViewById(R.id.city_textview);
+        ivPicture = (ImageView) findViewById(R.id.city_imageview);
+
+        buttonChange.setOnClickListener(changeViews);
     }
+
+    View.OnClickListener changeViews = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(!isFlipped) {
+                tvLabel.setText(R.string.label_eiffel);
+                ivPicture.setImageResource(R.drawable.eiffel);
+            }else{
+                tvLabel.setText(R.string.label_airport);
+                ivPicture.setImageResource(R.drawable.airport);
+            }
+            isFlipped = !isFlipped;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
